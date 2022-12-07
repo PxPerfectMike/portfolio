e3d8f9dc7f5f91d6e2e810196955339ab1958a0d
michael baker <56899037+PxPerfectMike@users.noreply.github.com>
2022-12-07 12:26:24 -0800

    game working single poage conditional render

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React, { useState } from 'react';
-import Navbar from './Components/Navigation/Navbar';
-import NavbarMobile from './Components/Navigation/NavbarMobile';
-import Box from '@mui/material/Box';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import GamePage from './Components/Game/GamePage';
-import NamePlate from './Components/NamePlate';
-import Home from './Components/Home';
 
 function App() {
-	const fullScreen = useMediaQuery('(min-width:600px)');
-	const shortScreen = useMediaQuery('(max-height:950px)');
-	const [pageIndex, setPageIndex] = useState(0);
+	const pageRendered = 0;
 
-	const mobileNavToApp = (pageIndex) => {
-		setPageIndex(pageIndex);
-	};
-
-	const navToApp = (pageIndex) => {
-		setPageIndex(pageIndex);
-	};
-
-	return (
-		<>
-			<div></div>
-		</>
-	);
+	return <>{pageRendered === 0 && <GamePage />}</>;
 }
 
 export default App;
